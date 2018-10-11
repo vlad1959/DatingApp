1f333da996c9351db912fb88618d6e3725994d69
Vladimir Shtessel <vshte@hotmail.com>
2018-10-10 20:03:16 -0500

    End of section 9

---
+++ DatingApp-SPA/src/app/_servicies/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+
+// token is sent with JWT angualr2 interceptor defined in app.module.ts
+/*
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
+};
+*/
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+constructor(private http: HttpClient) { }
+  getUsers(): Observable<User[]> {
+    // return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<User[]>(this.baseUrl + 'users');
+  }
+
+  getUser(id): Observable<User> {
+    // return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+}
--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Models
@@ -12,5 +14,16 @@ namespace DatingApp.API.Models
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
-    }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs  { get; set; }
+        public DateTime CreatedMyProperty { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interest { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }   
 }
